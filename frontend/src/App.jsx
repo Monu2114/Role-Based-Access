f74469301e3a2533ebae6236d8c62e19bcb97b8c
Monu_2114 <monu.nanabala@gmail.com>
2024-11-25 01:09:05 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-slate-200 h-screen	">
+        <div className="header">
+          <img
+            src="panda-sneak.jpg"
+            alt=""
+            className="w-16 h-16 rounded-full"
+          />
+          <h1 className="font-cursive font-">Pandagram</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
