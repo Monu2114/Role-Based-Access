67b413bef88d9c065b45bd7f16f78260aec26fa8
Monu_2114 <monu.nanabala@gmail.com>
2024-11-25 19:17:54 +0530

    same

--- frontend/src/components/WhoAreYou.jsx
+++ frontend/src/components/WhoAreYou.jsx
@@ -1,5 +1,5 @@
 import Card from "./Card";
-export default function Who() {
+export default function Who({ setRole }) {
   return (
     <div className="flex flex-col  bg-white mx-2 px-6 rounded-lg w-1/3 h-80 font-cursive p-6 shadow-xl space-y-4 px-2	">
       {/* Header */}
@@ -14,9 +14,9 @@ export default function Who() {
 
       {/* Cards Section */}
       <div className="flex justify-center gap-x-6">
-        <Card role="Admin" image="admin.jpg" />
-        <Card role="Manager" image="manager.jpg" />
-        <Card role="Guest" image="guest.jpeg" />
+        <Card role="Admin" setRole={setRole} image="admin.jpg" />
+        <Card role="Manager" setRole={setRole} image="manager.jpg" />
+        <Card role="Guest" setRole={setRole} image="guest.jpeg" />
       </div>
     </div>
   );
